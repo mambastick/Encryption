44c4383dcffe30bce3d5e7ea724295efb3725026
mamba <neelov.ev@mail.ru>
2023-10-02 16:58:42 +0300

    Создание проекта

---
+++ Encryption/EncryptionHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+
+namespace Encryption;
+
+public class EncryptionHelper
+{
+    public static byte[] GenerateRandomKey(int sizeInBytes)
+    {
+        using var rng = new RNGCryptoServiceProvider();
+        var key = new byte[sizeInBytes];
+        rng.GetBytes(key);
+        return key;
+    }
+
+    public static byte[] GenerateRandomIV(int sizeInBytes)
+    {
+        using var rng = new RNGCryptoServiceProvider();
+        var iv = new byte[sizeInBytes];
+        rng.GetBytes(iv);
+        return iv;
+    }
+}
