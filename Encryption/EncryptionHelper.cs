4b0add8aa77d13029679d609ad2e2b7ff0ea8ff0
Neelov Eugene <neelov.ev@mail.ru>
2023-10-04 15:14:25 +0300

    добавление методов для шифрования и дешифрования данных

--- Encryption/EncryptionHelper.cs
+++ Encryption/EncryptionHelper.cs
@@ -19,4 +19,38 @@ public class EncryptionHelper
         rng.GetBytes(iv);
         return iv;
     }
+    
+    public byte[] Encrypt(string plainText, byte[] key, byte[] iv)
+    {
+        using var aesAlg = Aes.Create();
+        aesAlg.Key = key;
+        aesAlg.IV = iv;
+
+        var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+        using var msEncrypt = new MemoryStream();
+        using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+        {
+            using (var swEncrypt = new StreamWriter(csEncrypt))
+            {
+                swEncrypt.Write(plainText);
+            }
+        }
+
+        return msEncrypt.ToArray();
+    }
+
+    public string Decrypt(byte[] cipherText, byte[] key, byte[] iv)
+    {
+        using var aesAlg = Aes.Create();
+        aesAlg.Key = key;
+        aesAlg.IV = iv;
+
+        var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+        using var msDecrypt = new MemoryStream(cipherText);
+        using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+        using var srDecrypt = new StreamReader(csDecrypt);
+        return srDecrypt.ReadToEnd();
+    }
 }
